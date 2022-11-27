18c93d46201f1063f452f751f4662821239de9b9
Programming Hero <programming.hero1@gmail.com>
2022-11-26 23:24:25 -0600

    third commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>some changes</p>
+        <p>some more changes</p>
         <a
           className="App-link"
           href="https://reactjs.org"
